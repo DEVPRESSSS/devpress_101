6df87f602ce0281848b4ee64b6cad227b09267fa
devpresss xmont <xmontemorjerald@gmail.com>
2026-04-07 07:00:04 +0800

    Added vercel.json

--- Devpress101/obj/Debug/net10.0/Devpress101.AssemblyInfo.cs
+++ Devpress101/obj/Debug/net10.0/Devpress101.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Devpress101")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2487dc9a51eed0d5d0780e293ed1e11e8e9666c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Devpress101")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Devpress101")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
