64af2c044d8b3997182a61a782435eb1c2c172f0
devpresss xmont <xmontemorjerald@gmail.com>
2026-04-07 07:39:02 +0800

    Release again

--- Devpress101/obj/Release/net10.0/Devpress101.AssemblyInfo.cs
+++ Devpress101/obj/Release/net10.0/Devpress101.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Devpress101")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+061ca07220ed2785cde871e0fa5b3cd6800a35c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+143d0225afad1aa3006e206bb89d24d5786ee60b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Devpress101")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Devpress101")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
